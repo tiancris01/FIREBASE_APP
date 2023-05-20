f06029d898276f518e4f2fd18d87288da190dfce
Tiancris <tiancris01@gmail.com>
2023-05-19 20:34:39 -0500

    basics screens

---
+++ lib/presentation/splash/onboarding.dart
@@ -0,0 +1,11 @@
+import 'package:firebase/presentation/home/home.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage();
+  }
+}
